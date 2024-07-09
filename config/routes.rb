ce521ea332ed1f75656b6ac4b2b279524f2acd45
yuushigure27 <yuushigure1227@gmail.com>
2024-07-09 14:59:41 +0000

    [create]

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,19 @@ Rails.application.routes.draw do
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "customer/registrations",
     sessions: 'customer/sessions'
-  
   }
   
-  # 顧客用
+  # ショップ用
   # URL /shops/sign_in ...
   devise_for :shops,skip: [:passwords], controllers: {
     registrations: "shop/registrations",
     sessions: 'shop/sessions'
   }
+  
+  # 商品管理用
+  resources :products
+  
+  
   
   # 管理者用
   # URL /admin/sign_in ...

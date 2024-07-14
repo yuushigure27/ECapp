922858fd78d7c7b71a1548ed08d488cf59f07344
yuushigure27 <yuushigure1227@gmail.com>
2024-07-14 12:47:45 +0000

    [update]shop ログイン後の遷移場所

--- app/controllers/shop/sessions_controller.rb
+++ app/controllers/shop/sessions_controller.rb
@@ -3,25 +3,21 @@
 class Shop::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  protected
+  
+  def after_sign_in_path_for(resource)
+    shop_top_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    new_shop_session_path
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :encrypted_password) }
+  end
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  
 end

33971782fa92c0594949be8d67791dc8bfe77959
yuushigure27 <yuushigure1227@gmail.com>
2024-07-24 14:56:08 +0000

    [update]shop 商品登録フォーム修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  
+  # 在庫数が0以上であることを確認
+  validates :stock, numericality: { greater_than_or_equal_to: 0 }
+  # カテゴリが重複しないことを確認
+  validates :category, uniqueness: true
+  validates :name, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
 end
